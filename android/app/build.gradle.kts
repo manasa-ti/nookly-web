4bf6b9022ef3e11a9c4d29d66034b6e6918960c5
manasa-ti <manasatimalapura@gmail.com>
2025-07-31 18:14:50 +0530

    version upgraded

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
     }
 
     signingConfigs {
