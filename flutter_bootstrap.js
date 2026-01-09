20c12945ee8f5df58a01377d237fae5765cc8a65
manasa-ti <manasatimalapura@gmail.com>
2026-01-09 12:56:27 +0530

    Skip Firebase Messaging on web to fix 404 error - 2026-01-09 12:56:27

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2319489611"
+    serviceWorkerVersion: "2748061664"
   }
 });
