96967ac895d3d9573d48e96874af81436d865fd8
manasa-ti <manasatimalapura@gmail.com>
2026-01-09 17:44:08 +0530

    disabled dio perf/analytics interceptors to test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3001888177"
+    serviceWorkerVersion: "932592111"
   }
 });
